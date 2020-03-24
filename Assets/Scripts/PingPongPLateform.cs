2b2eef0245552f851fa99e8c62d0a7c49c666bd4
Nathan CHEVALIER <nathanchevalier.lechatelet@gmail.com>
2020-03-24 23:25:46 +0100

    Project Init

---
+++ Assets/Scripts/PingPongPLateform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongPLateform : MonoBehaviour
+{
+    public float start = 0f;
+    public float speed = 2f;
+    public float length = 3f;
+         // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.PingPong(Time.time * speed, length) + start, transform.position.y, transform.position.z);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.collider.transform.SetParent(transform);
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        collision.collider.transform.SetParent(null);
+    }
+}
---
+++ Assets/Scripts/arriere.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arriere : MonoBehaviour
+{
+	public Vector3 safepos;
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject)
+		{
+			other.transform.position = safepos ;
+		}
+		
+	}
+}
