74249e4bb79e02a44fa2492530b8d788e85e0809
Emil TOULOUSE <emil.toulouse@epita.fr>
2020-04-02 12:31:01 +0200

    Level Selector

---
+++ Assets/Scripts/GoToLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToLevel : MonoBehaviour
+{
+    int BaseLevel = 1;
+
+    public void LoadLevel(int level)
+    {
+        SceneManager.LoadScene(level + BaseLevel);
+    }
+    public void NextPage()
+    {
+
+    }
+
+    public void PreviousPage()
+    {
+
+    }
+}
