cd850532a2d058ca3c07ff75ae07e4470f1d7749
Emil TOULOUSE <emil.toulouse@epita.fr>
2020-04-04 19:39:35 +0200

    Added Animated Leila in Level

--- Assets/Scripts/LunchPadPlateform.cs
+++ Assets/Scripts/LunchPadPlateform.cs
@@ -8,12 +8,8 @@ public class LunchPadPlateform : MonoBehaviour
  
     public void OnCollisionEnter(Collision col)
     {
-        print("Collision Detected");
         col.rigidbody.AddForce(_force);
     }
-    public void OnCollisionExit(Collision collision)
-    {
-        collision.rigidbody.AddForce( 0,0,0);
-    }
+    
 
 }
