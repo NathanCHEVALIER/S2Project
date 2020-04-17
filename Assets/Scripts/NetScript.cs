78e3202bcb4aa471df9c7e2c59a53f6c6e6782a3
Nathan CHEVALIER <nathanchevalier.lechatelet@gmail.com>
2020-04-17 12:37:17 +0200

    Temp commit

---
+++ Assets/Scripts/NetScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetScript : MonoBehaviour
+{
+    public GameObject lobbyCam;
+    public Transform spawnPoint;
+    public Text NetStatus;
+
+    public const string Version = "1.101";
+    public const string RoomName = "Multiplayer";
+
+    public string playerPrefabName = "Player";
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(Version);
+    }
+
+    void Update()
+    {
+        NetStatus.text = PhotonNetwork.connectionStateDetailed.ToString();
+    }
+
+    public override void OnConnectionFail(DisconnectCause cause)
+    {
+        print("Connection failed!" + cause.ToString());
+    }
+
+    public override void OnJoinedLobby()
+    {
+        RoomOptions roomOptions = new RoomOptions() {IsVisible = false, MaxPlayers = 2};
+        PhotonNetwork.JoinOrCreateRoom(RoomName, roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        lobbyCam.SetActive(false);
+        GameObject player = PhotonNetwork.Instantiate(playerPrefabName, spawnPoint.position, spawnPoint.rotation, 0);
+    }
+    
+    public override void OnPhotonPlayerConnected(PhotonPlayer newPlayer)
+    {
+        print("New Player Connected");
+    }
+    
+    public override void OnPhotonPlayerDisconnected(PhotonPlayer otherPlayer)
+    {
+        print("Player DISconnected");
+    }
+}
---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class NetworkPlayer : Photon.MonoBehaviour
+{
+    public GameObject localCam;
+    
+    void Start()
+    {
+        if (!photonView.isMine)
+        {
+            localCam.SetActive(false);
+
+            MonoBehaviour[] scripts = GetComponents<MonoBehaviour>();
+
+            for (int i = 0; i < scripts.Length; i++)
+            {
+                if (scripts[i] is NetworkPlayer) continue;
+                else if (scripts[i] is PhotonView) continue;
+
+                scripts[i].enabled = false;
+            }
+        }
+    }
+}
