81a0aca1cc8a74f92184a4cd576103c733719a7b
Melinda <melinda.marmot@epita.fr>
2020-05-21 12:20:50 +0200

    Checkpoint

---
+++ Checkpoint/Assets/CheckPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+
+	Deathzone deathzone;
+
+	private void Start()
+	{
+		deathzone = GameObject.Find("Deathzone").GetComponent< Deathzone>();
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if(other.transform.gameObject.tag == "Player")
+		{
+			deathzone.respawnPositions = gameObject.transform.position;
+			Destroy(gameObject);
+		}
+	}
+}
