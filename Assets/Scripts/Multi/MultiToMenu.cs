93cc47a6a607905a58361946f9518fdaaaa326ef
Nathan CHEVALIER <nathanchevalier.lechatelet@gmail.com>
2020-05-24 14:58:26 +0200

    Exit Multi Improvment

---
+++ Assets/Scripts/Multi/MultiToMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiToMenu : MonoBehaviour
+{
+    public void ExitMenu()
+    {
+        NetworkManager NetManager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+        NetManager.BackToMenu();
+    }
+}
